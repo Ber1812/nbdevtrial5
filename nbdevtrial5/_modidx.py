16360d5da81b5bf6d6595c26d167b1dd2d9d1043
Ber1812 <mendesdado@gmail.com>
2022-10-23 13:36:03 +0100

    Add `can_you_count`

--- nbdevtrial5/_modidx.py
+++ nbdevtrial5/_modidx.py
@@ -5,4 +5,6 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://Ber1812.github.io',
                 'git_url': 'https://github.com/Ber1812/nbdevtrial5',
                 'lib_path': 'nbdevtrial5'},
-  'syms': {'nbdevtrial5.core': {'nbdevtrial5.core.foo': ('core.html#foo', 'nbdevtrial5/core.py')}}}
+  'syms': { 'nbdevtrial5.core': {'nbdevtrial5.core.foo': ('core.html#foo', 'nbdevtrial5/core.py')},
+            'nbdevtrial5.first_module': { 'nbdevtrial5.first_module.can_you_count': ( 'first_module.html#can_you_count',
+                                                                                      'nbdevtrial5/first_module.py')}}}
